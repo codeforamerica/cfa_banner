728eba87a3a3660df27e7b62e5c66fa231d522f0
Jesse Bounds <jesse@rebounds.net>
2012-05-22 10:50:58 -0700

    fixed url references

--- banner.js
+++ banner.js
@@ -1,5 +1,5 @@
 if(window.onload){
-    window.other_onloads = window.onloads;
+    var other_onloads = window.onloads; 
 }
 
 window.onload = function(){
@@ -24,8 +24,8 @@ window.onload = function(){
         var banner_div = document.createElement('div');
         banner_div.setAttribute("id", "cfa_corner_ribbon");
         banner_div.setAttribute("style", "position:absolute; width:150px; "+
-                                "height:150px; top:0px; left:0px;z-index:100000; "+
-                                "background:url(http://dthompson.github.com/cfa_banner/images/cfa_ribbon.png);");
+                                "height:150px; top:0px; left:0px; "+
+                                "background:url(images/cfa_ribbon.png); z-index:100000 ");
         banner_forkme_link.setAttribute("style", banner_div.getAttribute("style"));
         banner_div.appendChild(banner_forkme_link);
         document.getElementsByTagName("body")[0].appendChild(banner_div);
@@ -42,8 +42,8 @@ window.onload = function(){
         var banner_div = document.createElement('div');
         banner_div.setAttribute("id", "cfa_flag");
         banner_div.setAttribute("style", "position:absolute; width:238px; "+
-                                "height:321px; top:-200px; right:100px;z-index:100000;cursor:pointer; "+
-                                "background:url(http://dthompson.github.com/cfa_banner/images/cfa_flag.png); ");
+                                "height:321px; top:-200px; right:200px; "+
+                                "background:url(images/cfa_flag.png); z-index:100000;cursor:pointer; ");
         document.getElementsByTagName("body")[0].appendChild(banner_div);
 
         banner_div.addEventListener("mouseover", function(event){
